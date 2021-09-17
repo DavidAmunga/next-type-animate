f38204d6250bb98c222fd674578571239ee51629
David Amunga <david.onyimbo@gmail.com>
2021-09-17 12:07:18 +0300

    Setup Tailwind Colors,HeroIcons and Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green:colors.green
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
